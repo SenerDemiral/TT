835f1087b908bbc9db8d5b0abfa74514211920b6
SenerDemiral <sener.demiral@gmail.com>
2016-12-06 00:38:17 +0300

    #4

--- TT/TurnuvaTakimlarJson.json.cs
+++ TT/TurnuvaTakimlarJson.json.cs
@@ -7,19 +7,26 @@ namespace TT
     {
         public void RefreshData(string turnuvaID)
         {
+             
             var turnuva = DbHelper.FromID(DbHelper.Base64DecodeObjectID(turnuvaID));
-            Takimlar = Db.SQL("SELECT o FROM TurnuvaTakim o where o.Turnuva = ?", turnuva);
-            var taks = Db.SQL<TTDB.TurnuvaTakim>("SELECT o FROM TurnuvaTakim o where o.Turnuva = ?", turnuva).ToArray();
+            //Takimlar = Db.SQL("SELECT o FROM TurnuvaTakim o where o.Turnuva = ?", turnuva);
+            //var taks = Db.SQL<TTDB.TurnuvaTakim>("SELECT o FROM TurnuvaTakim o where o.Turnuva = ?", turnuva).ToArray();
+            Takimlar.Data = Db.SQL<TTDB.TurnuvaTakim>("SELECT o FROM TurnuvaTakim o WHERE o.Turnuva = ?", turnuva).OrderByDescending(x => x.Ozet.Puan);
 
+            //TurnuvaTakimlarJson page = new TurnuvaTakimlarJson();
+            //page.Takimlar.Data = Db.SQL<TTDB.TurnuvaTakim>("SELECT o FROM TurnuvaTakim o WHERE o.Turnuva = ?", turnuva).OrderByDescending(x => x.Ozet.Puan);
+            //var abc = Takimlar.OrderBy(x => x.Ozet.Puan);
+            /*
             var sort = from s in taks
                        orderby  s.Ozet.Puan descending
                        select s;
 
-            //Takimlar = sort;  ??????
+            var abc = Takimlar.OrderBy(x => x.Ozet.Puan);
+
             foreach (var item in sort) {
                 //item.Ozet.Puan;   //deneme github  
                
-            }
+            }*/
         }
 
     }
