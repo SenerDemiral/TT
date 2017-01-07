f696ebbbd7479b398414f821f80906866094cf49
Administrator <Administrator@VMB11EE2D>
2017-01-07 12:13:56 +0000

    Turkısh

--- TTclient/TurnuvaPage.json.cs
+++ TTclient/TurnuvaPage.json.cs
@@ -58,7 +58,7 @@ namespace TTclient
 					takim.TurnuvaID = ID;
 					var trn = (TTDB.Turnuva)DbHelper.FromID(DbHelper.Base64DecodeObjectID(ID));
 
-					takim.TurnuvaInfo = $"{trn.Ad} Tak�m Sonu�lar�";
+					takim.TurnuvaInfo = $"{trn.Ad} Takım Sonuçları";
 					takim.Data = null;
 					RecentTakim = takim;
 				}
@@ -74,7 +74,7 @@ namespace TTclient
 					oyuncu.TurnuvaID = ID;
 					var trn = (TTDB.Turnuva)DbHelper.FromID(DbHelper.Base64DecodeObjectID(ID));
 
-					oyuncu.TurnuvaInfo = $"{trn.Ad} Oyuncu Sonu�lar�";
+					oyuncu.TurnuvaInfo = $"{trn.Ad} Oyuncu Sonuçları";
 					oyuncu.Data = null;
 					RecentOyuncu = oyuncu;
 				}
